b812a0b26cf811769d86a87e6346361da925b4dc
arturlazarenkoemnify <artur.lazarenko@iternal.group>
2022-12-29 04:32:37 +0100

    feat: completed

--- xml_converter/api.py
+++ xml_converter/api.py
@@ -1,17 +1,31 @@
-from xml.etree import ElementTree
-
 from rest_framework.decorators import action
 from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
+from rest_framework.request import Request
 from rest_framework.viewsets import ViewSet
+from rest_framework.renderers import JSONRenderer
 
+from xml_converter.utils.xml_utils import xml_to_dict, parse_xml
 
 
 class ConverterViewSet(ViewSet):
     # Note this is not a restful API
     # We still use DRF to assess how well you know the framework
     parser_classes = [MultiPartParser]
+    renderer_classes = [JSONRenderer]
 
     @action(methods=["POST"], detail=False, url_path="convert", url_name="convert")
-    def convert(self, request, **kwargs):
-        return Response({})
+    def convert(self, request: Request, **kwargs) -> Response:
+        if "file" not in request.data:
+            return Response({"error": "No file provided"}, status=400)
+
+        root = parse_xml(request.data["file"])
+        if root is None:
+            # XML file could not be parsed
+            return Response({"error": "Invalid XML file"}, status=400)
+
+        # Convert the Python dictionary to a JSON object
+
+        json_data = xml_to_dict(root)
+
+        return Response(json_data, content_type="application/json")
--- xml_converter/views.py
+++ xml_converter/views.py
@@ -1,10 +1,21 @@
-from django.http import JsonResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
 
-def upload_page(request):
-    if request.method == 'POST':
-        # TODO: Convert the submitted XML file into a JSON object and return to the user.
-        return JsonResponse({})
+from xml_converter.api import ConverterViewSet
 
+@api_view(['POST', 'GET'])
+def upload_page(request: Request) -> HttpResponse:
+    """
+    The api_view decorator allows us to modify the WSGI request
+    so that it can be handled by Django REST framework.
+    It also ensures that the response is properly formatted according
+    to the Django REST framework standards.
+
+    In this case, the decorator allows us to use the ConverterViewSet().convert method to handle the POST request
+    """
+    if request.method == "POST":
+        return ConverterViewSet().convert(request)
     return render(request, "upload_page.html")
